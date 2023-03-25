919b5c7fc46eb6bfe2602bfc5179a512108fa10f
Robert West <robert.david.west@gmail.com>
2023-03-25 08:43:19 -0400

    refactored song api, prepping for different request types

--- pages/api/song/[id].tsx
+++ pages/api/song/[id].tsx
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
 
 import { getSession } from "next-auth/react";
+import { Session } from "next-auth";
 
 const prisma = new PrismaClient();
 
@@ -11,42 +12,8 @@ export default async function handler(
 ) {
   const session = await getSession({ req });
   if (session) {
-    const { id } = req.query;
-    const idNum = Number(id);
-
-    const { user } = session;
-    const email = user?.email as string;
-    const dbUser = await prisma.user.findUnique({
-      where: { email },
-      select: { id: true, email: true },
-    });
-    const userId = dbUser?.id as number;
-
-    const song = await prisma.song.findUnique({
-      where: { id: idNum },
-      select: {
-        id: true,
-        name: true,
-        audioUrl: true,
-        artist: true,
-        duration: true,
-        createdAt: true,
-        sections: true,
-        userId: true,
-      },
-    });
-    if (!song) {
-      res
-        .status(404)
-        .json({ error: { message: "This page does not exist", status: 404 } });
-    }
-    if (song?.userId !== userId) {
-      res.status(401).json({
-        error: { message: "cannot access other users data", status: 401 },
-      });
-    }
-    console.log("song", song);
-    res.status(200).json({ song });
+    if (req.method === "GET") getSong(session, req, res);
+    else if (req.method === "POST") postSong(session, req, res);
   } else {
     // Not Signed in
     res.status(401).json({
@@ -54,3 +21,51 @@ export default async function handler(
     });
   }
 }
+
+async function getSong(
+  session: Session,
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { id } = req.query;
+  const idNum = Number(id);
+
+  const { user } = session;
+  const email = user?.email as string;
+  const dbUser = await prisma.user.findUnique({
+    where: { email },
+    select: { id: true, email: true },
+  });
+  const userId = dbUser?.id as number;
+
+  const song = await prisma.song.findUnique({
+    where: { id: idNum },
+    select: {
+      id: true,
+      name: true,
+      audioUrl: true,
+      artist: true,
+      duration: true,
+      createdAt: true,
+      sections: true,
+      userId: true,
+    },
+  });
+  if (!song) {
+    res
+      .status(404)
+      .json({ error: { message: "This page does not exist", status: 404 } });
+  }
+  if (song?.userId !== userId) {
+    res.status(401).json({
+      error: { message: "cannot access other users data", status: 401 },
+    });
+  }
+  res.status(200).json({ song });
+}
+
+async function postSong(
+  session: Session,
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {}
