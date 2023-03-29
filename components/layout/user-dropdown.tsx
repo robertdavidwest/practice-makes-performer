09c6796ff342b337319c33013a4dc4e1fa540a40
Robert West <robert.david.west@gmail.com>
2023-03-29 13:13:29 -0400

    dont show dashboard option

--- components/layout/user-dropdown.tsx
+++ components/layout/user-dropdown.tsx
@@ -28,13 +28,13 @@ export default function UserDropdown() {
               <LayoutDashboard className="h-4 w-4" />
               <p className="text-sm">Dashboard</p>
             </Link> */}
-            <button
+            {/* <button
               className="relative flex w-full cursor-not-allowed items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100"
               disabled
             >
               <LayoutDashboard className="h-4 w-4" />
               <p className="text-sm">Dashboard</p>
-            </button>
+            </button> */}
             <button
               className="relative flex w-full items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100"
               onClick={() => signOut({ redirect: false })}
