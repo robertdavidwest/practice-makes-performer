45b1e4bc7717ca872f947f7e3d14d8c8010d94bf
Robert West <robert.david.west@gmail.com>
2023-04-10 09:01:45 -0400

    removing sign up button, its more trouble than its worth if users cant even sign up this way

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,16 +60,10 @@ export default function Home() {
           variants={FADE_DOWN_ANIMATION_VARIANTS}
         >
           <Link
-            className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-black bg-black px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-black"
+            className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-black bg-purple-700 px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-black"
             href="/demoSongs/0"
           >
             <p>Try it out!</p>
-          </Link>
-          <Link
-            className="group flex max-w-fit cursor-pointer items-center justify-center space-x-2 rounded-full border border-black bg-purple-700 px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-black"
-            href="/api/auth/signin"
-          >
-            <p>Create An Account</p>
           </Link>
         </motion.div>
       </motion.div>
